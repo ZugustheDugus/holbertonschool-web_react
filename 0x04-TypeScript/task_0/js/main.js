df4dae88d78d21b85f47df23ee5ab36045411560
ZugustheDugus <h1cooper2019@protonmail.com>
2022-05-12 20:30:38 -0500

    Asdf

--- 0x04-TypeScript/task_0/js/main.js
+++ 0x04-TypeScript/task_0/js/main.js
@@ -10,5 +10,12 @@ var student2 = {
     age: 12,
     location: 'San Diego'
 };
-var studentsList = [student1, student2];
-console.log(studentsList);
+var studentList = [student1, student2];
+var table = document.createElement('table');
+for (var _i = 0, studentList_1 = studentList; _i < studentList_1.length; _i++) {
+    var student = studentList_1[_i];
+    var row = table.insertRow();
+    row.insertCell().innerHTML = student.firstName;
+    row.insertCell().innerHTML = student.location;
+}
+document.body.appendChild(table);
