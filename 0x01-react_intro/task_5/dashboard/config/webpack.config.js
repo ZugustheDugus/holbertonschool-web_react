6d192e91f88f82dab3a34bcda536b8454beb0a34
ZugustheDugus <h1cooper2019@protonmail.com>
2023-01-11 15:42:01 -0600

    task 8

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  devServer: {
+    static: path.join(__dirname, '../dist'),
+    hot: true,
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        use: ["file-loader", "image-webpack-loader"],
+      }
+    ],
+  },
+  devtool: 'inline-source-map',
+};
