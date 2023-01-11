a18b2306835a3d0b73f7b117a24b9ebcef7a96ed
ZugustheDugus <h1cooper2019@protonmail.com>
2023-01-11 11:05:45 -0600

    task 1

---
+++ 0x01-react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Notifications.css";
+
+function notify() {
+  return (
+  <div class="Notifications">
+    <p>Here is a list of notificaitons</p>
+  </div>
+  )
+}
+
+module.exports = {
+  notify
+}
