2c23412ff8e060cfe1fca6090e56fff4691dfcfa
ZugustheDugus <h1cooper2019@protonmail.com>
2023-01-25 14:56:40 -0600

    task 8

---
+++ 0x01-React_Immutable/8-seq.js
@@ -0,0 +1,18 @@
+import { Seq } from 'immutable';
+
+export default function printBestStudents(obj) {
+  const student = Seq(obj).filter((v) => v.score > 70).toJS();
+  let firstName;
+  let lastName;
+
+  Object.keys(student).map((key) => {
+    firstName = student[key].firstName;
+    lastName = student[key].lastName;
+    firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+    lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+    student[key].firstName = firstName;
+    student[key].lastName = lastName;
+    return 1;
+  });
+  console.log(student);
+}
