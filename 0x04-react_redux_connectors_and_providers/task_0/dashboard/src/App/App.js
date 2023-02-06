151870470bfe01f00c1e3f9a2d25ec58e9cc1bff
ZugustheDugus <h1cooper2019@protonmail.com>
2023-02-06 17:55:30 -0600

    tasks 0-2

--- 0x04-react_redux_connectors_and_providers/task_0/dashboard/src/App/App.js
+++ 0x04-react_redux_connectors_and_providers/task_0/dashboard/src/App/App.js
@@ -1,164 +1,159 @@
-import PropTypes from 'prop-types';
-import Notifications from '../Notifications/Notifications'
 import React from 'react';
-import ReactDOM from 'react-dom'
-import Header from '../Header/Header'
-import Footer from '../Footer/Footer'
-import Login from '../Login/Login'
+import PropTypes from 'prop-types';
+import { css, StyleSheet } from 'aphrodite';
+import BodySection from '../BodySection/BodySection';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
 import CourseList from '../CourseList/CourseList';
-import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom'
-import BodySection from '../BodySection/BodySection'
-import { StyleSheet, css } from 'aphrodite';
-import userContext from './AppContext';
-
-
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Notifications from '../Notifications/Notifications';
+import { getLatestNotification } from '../utils/utils';
+import AppContext from './AppContext';
+import { connect } from 'react-redux';
 
 const listCourses = [
-  {id: 1, name: 'ES6', credit: 60},
-  {id: 2, name: 'Webpack', credit: 20},
-  {id: 3, name: 'React', credit: 40}
-]
+  { id: '1', name: 'ES6', credit: 60 },
+  { id: '2', name: 'Webpack', credit: 20 },
+  { id: '3', name: 'React', credit: 40 },
+];
 
-const listNotifications = [
-  {
-    value: 'Someone wants to connect!',
-    id: 0,
-    type: 'default',
-  },
-  {
-    value: 'Employer wants to reach out',
-    id: 1,
-    type: 'urgent',
-  },
-  {
-    value: '',
-    id: 2,
-    type: 'urgent',
-    html: {__html: 'This is a string'}
-  },
-]
+export class App extends React.Component {
+  static contextType = AppContext;
 
-const bodyStyles = StyleSheet.create({
-  body: {width: '100%'}
-})
-const footerStyles = StyleSheet.create({
-  footer: {
-    position: 'fixed',
-    left: '0',
-    bottom: '0',
-    width: '100%',
-    textAlign: 'center',
-    borderTop: '#E0354B solid'
-  }
-})
-
-class App extends React.Component {
   constructor(props) {
     super(props);
-    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
-    this.handleHideDrawer = this.handleHideDrawer.bind(this);
-    this.logIn = this.logIn.bind(this);
-    this.logOut = this.logOut.bind(this);
-    this.markNotificationAsRead = this.markNotificationAsRead.bind(this);
     this.state = {
       displayDrawer: false,
-      user: {
-        email: '',
-        password: '',
-        isLoggedIn: false
-      },
-      logOut: this.logOut,
-      listNotifications: listNotifications
+      user: AppContext._currentValue.user,
+      logout: AppContext._currentValue.logout,
+      listNotifications: [
+        { id: 1, type: 'default', value: 'New course available' },
+        { id: 2, type: 'urgent', value: 'New resume available' },
+        { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+      ],
     };
+    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
+    this.handleHideDrawer = this.handleHideDrawer.bind(this);
+    this.logoutListener = this.logoutListener.bind(this);
+    this.markNotificationAsRead = this.markNotificationAsRead.bind(this);
+    this.login = this.login.bind(this);
+    this.state.logout = this.state.logout.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.componentWillUnmount = this.componentWillUnmount.bind(this);
   }
-  
-  handleDisplayDrawer = () => {
+
+  handleDisplayDrawer() {
     this.setState({ displayDrawer: true });
   }
-  handleHideDrawer = () => {
+
+  handleHideDrawer() {
     this.setState({ displayDrawer: false });
   }
 
-  logIn = (email, password) => {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeydown);
+    document.addEventListener('keydown', this.logoutListener);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeydown);
+    document.removeEventListener('keydown', this.logoutListener);
+  }
+
+  login(email, password) {
     this.setState({
       user: {
         email,
         password,
-        isLoggedIn: true
+        isLoggedIn: true,
       }
     });
-  }
-  
-  logOut = () => {
-    this.setState({
-      user: {
-        email: '',
-        password: '',
-        isLoggedIn: false
-      }
-    })
+    this.context.user = {
+      email,
+      password,
+      isLoggedIn: true,
+    };
   }
 
-  markNotificationAsRead = (id) => {
-    const listNotifications = [...this.state.listNotifications];
-    for (let i = 0; i < listNotifications.length; i++) {
-      if (listNotifications[i].id === id) {
-        listNotifications.splice(i, 1);
-      }
+  logoutListener(event) {
+    if (event.ctrlKey && event.key === 'h') {
+      alert('Logging you out');
+      this.state.logout();
     }
-    this.setState({
-      listNotifications: listNotifications
-    })
-  }
+  };
 
-  componentDidMount() {
-    document.addEventListener('keydown', (event) => {
-      if ((event.metaKey || event.ctrlKey) && event.key === 'h') {
-        window.alert('Logging you out');
-        this.logOut();
-      }
+  markNotificationAsRead(id) {
+    let ln = this.state.listNotifications
+    this.setState({
+      listNotifications: ln.filter((note) => note.id !== id),
     });
-  }
-  
-  
-  
+  };
+
   render() {
-    const handleHideDrawer = this.handleHideDrawer;
-    const handleDisplayDrawer = this.handleDisplayDrawer;
-    const { displayDrawer, user, logOut } = this.state;
-    if (user.isLoggedIn) {
-      this.screen = <BodySectionWithMarginBottom title="Course list"><CourseList listCourses={listCourses}/></BodySectionWithMarginBottom>;
-    } else {
-      this.screen = <BodySectionWithMarginBottom title="Log in to continue"><Login logIn={this.logIn} /></BodySectionWithMarginBottom>;
-    }
+    const style = StyleSheet.create({
+      body: {
+        padding: '2% 3%',
+        height: '480px',
+        '@media (max-width: 900px)': {
+          display: this.state.displayDrawer ? 'none' : 'block',
+        },
+      },
+      footer: {	
+        borderTop: '3px #e0354b solid',	
+        position: 'relative',	
+        bottom: 0,	
+        fontStyle: 'italic',	
+        // width: '100%',
+        textAlign: 'center',
+      },	
+    });
+
     return (
-    <userContext.Provider value={{
-      user: this.state.user,
-      logOut: this.state.logOut
-    }}>
-      <Notifications
-      markNotificationAsRead={this.markNotificationAsRead}
-      displayDrawer={displayDrawer}
-      listNotifications={this.state.listNotifications}
-      handleDisplayDrawer={handleDisplayDrawer}
-      handleHideDrawer={handleHideDrawer} />
-      <div className="App">
-        <Header />
-        <div className={css(bodyStyles.body)}>
-          {this.screen}
+      <AppContext.Provider value={{ user: this.state.user, logout: this.state.logout }}>
+        <div className='App'>
+          <Notifications
+            displayDrawer={this.state.displayDrawer}
+            listNotifications={this.state.listNotifications}
+            handleHideDrawer={this.handleHideDrawer}
+            handleDisplayDrawer={this.handleDisplayDrawer}
+            markNotificationAsRead={this.markNotificationAsRead}
+          />
+          <Header />
+          <div className={`App-body ${css(style.body)}`}>
+            {this.state.user.isLoggedIn ?
+              <BodySectionWithMarginBottom title='Course list'>
+                <CourseList listCourses={listCourses} />
+              </BodySectionWithMarginBottom>
+              : <BodySectionWithMarginBottom title='Log in to continue'>
+                <Login login={this.login} />
+              </BodySectionWithMarginBottom>
+            }
+            <BodySection title='News from the School'>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus cumque magnam, consectetur soluta autem at facilis voluptatem aliquid dolor, unde perferendis laborum expedita, ad nihil? Adipisci aperiam sapiente quod perferendis.</p>
+            </BodySection>
+          </div>
+          <footer className={css(style.footer)}>
+            <Footer />
+          </footer>
         </div>
-        <div>
-          <BodySection title="News from the School">
-            <p>TULSA, Okla. — The Muscogee Creek National Council approved an $8.2 million dollar project agreement on Thursday with the City of Tulsa, the City of Jenks, and INGOG to create a new lake in the Arkansas River.</p>
-          </BodySection>
-        </div>
-        <div className={css(footerStyles.footer)}>
-          <Footer />
-        </div>
-      </div>
-    </userContext.Provider>
-    )
+      </AppContext.Provider >
+    );
   }
 }
 
-export default App;
+export const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.get('isUserLoggedIn'),
+  };
+};
+
+connect(mapStateToProps)(App);
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+};
--- 0x04-react_redux_connectors_and_providers/task_0/dashboard/src/App/App.test.js
+++ 0x04-react_redux_connectors_and_providers/task_0/dashboard/src/App/App.test.js
@@ -1,108 +1,157 @@
-import { shallow, mount } from 'enzyme';
 import React from 'react';
-import App from './App';
-import sinon from 'sinon';
+import { App, mapStateToProps } from './App';
+import { mount } from 'enzyme';
+import { assert } from 'chai';
 import { StyleSheetTestUtils } from 'aphrodite';
-jest.useFakeTimers();
+import { fromJS } from 'immutable';
 
+global.console.log = jest.fn()
 
-window.alert = sinon.spy()
+describe('App Renders', () => {
 
-
-describe('<App />', () => {
   beforeEach(() => {
     StyleSheetTestUtils.suppressStyleInjection();
   });
+
   afterEach(() => {
     StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
   });
-  it("renders entire App without crashing", () => {
-    shallow(<App />);
+
+  const alert = jest.spyOn(window, 'alert').mockImplementation((text) => console.log(text));
+
+  const app = mount(<App />);
+  const logout = jest.spyOn(app.state(), 'logout');
+
+  const header = app.find('Header');
+  const body = app.find('.App-body');
+  const footer = app.find('Footer');
+  const notificationsRender = app.find('Notifications').render();
+  const headerRender = app.find('Header').render();
+  const loginRender = app.find('Login').render();
+  const courseListRender = app.find('CourseList');
+  const footerRender = app.find('Footer').render();
+
+  it('without crashing', () => {
+    assert.equal(app.length, 1);
   });
-  it("Contains Notifications Component", () => {
-    const SApp = shallow(<App />);
-    expect(SApp.find('Notifications').length).toEqual(1);
+
+  it('the header', () => {
+    assert.equal(header.length, 1);
   });
-  it("Contains Header Component", () => {
-    const SApp = shallow(<App />);
-    expect(SApp.find('Header').length).toEqual(1);
+
+  it('the body', () => {
+    assert.equal(body.length, 1);
   });
-  it("Contains Login Component", () => {
-    const SApp = shallow(<App />);
-    expect(SApp.find('Login').length).toEqual(1);
+
+  it('the footer', () => {
+    assert.equal(footer.length, 1);
   });
-  it("Contains Footer Component", () => {
-    const SApp = shallow(<App />);
-    expect(SApp.find('Footer').length).toEqual(1);
+
+  it('children that render correctly', () => {
+    assert.equal(notificationsRender.find('.menuItem').length, 1);
+    assert.equal(notificationsRender.length, 1);
+    assert.equal(notificationsRender.find('NotificationItem').length, 0);
+    assert.equal(notificationsRender.children().length, 1);
+    assert.equal(headerRender.children().length, 2);
+    assert.equal(loginRender.length, 2);
+    assert.equal(footerRender.length, 1);
   });
-  it("Does not contain CourseList Component", () => {
-    const SApp = shallow(<App />);
-    expect(SApp.find('CourseList').length).toEqual(0);
+
+  it('an alert and calls the function logout when ctrl-h is pressed', () => {
+    document.dispatchEvent(new KeyboardEvent("keydown", { ctrlKey: true, key: 'h' }));
+    expect(alert).toHaveBeenCalled();
+    expect(alert).toHaveBeenCalledWith('Logging you out');
+    expect(logout).toHaveBeenCalled();
   });
-  it("Contains CourseList Component when logged in", () => {
-    const SApp = shallow(<App />);
-    SApp.instance().logIn()
-    expect(SApp.find('CourseList').length).toEqual(1);
+
+  it('app.state.displayDrawer is false, and true after handleDisplayDrawer', () => {
+    assert.equal(app.state().displayDrawer, false);
+    app.instance().handleDisplayDrawer();
+    assert.equal(app.state().displayDrawer, true);
   });
-  it("Does not contain Login Component when logged in", () => {
-    const SApp = shallow(<App />);
-    SApp.instance().logIn()
-    expect(SApp.find('Login').length).toEqual(0);
+
+  it('app.state.displayDrawer is true, and false after handleHideDrawer', () => {
+    app.instance().handleHideDrawer();
+    assert.equal(app.state().displayDrawer, false);
+  });
+
+  it('does not log in when enableSubmit = false and form is submitted', () => {
+    app.find('form').simulate('submit')
+    expect(alert).toHaveBeenCalledWith('Please enter email and password to proceed');
+  });
+
+  it('logs in when email & password are entered and form is submitted', () => {
+    app.find('form').find('input').first().simulate('change', { target: { value: 'a@b' }});
+    app.find('input').at(1).simulate('change', { target: { value: 'c' }});
+    app.find('form').simulate('submit')
+    assert.notStrictEqual(app.state().user, {
+      email: 'a@b',
+      password: 'c',
+      isLoggedIn: true,
+    })
+  });
+
+  it('updated Notifications when one is clicked, will run markNotificationAsRead', () => {
+    app.find('.menuItem').simulate('click');
+    assert.equal(app.find('NotificationItem').length, 3);
+    app.find('NotificationItem').first().simulate('click');
+    assert.equal(app.find('NotificationItem').length, 2);
+  });
+
+  it('NOT the CourseList', () => {
+    assert.equal(courseListRender.length, 0);
   });
 });
 
-describe('Keydown event listener works as planned', () => {
-  let keydown = {};
-  let logout = jest.fn();
-
+describe('Logged in App Renders', () => {
   beforeEach(() => {
     StyleSheetTestUtils.suppressStyleInjection();
-    document.addEventListener = jest.fn((event, cb) => {
-          keydown[event] = cb;
-        });
-  })
-  afterEach(function() {
+  });
+
+  afterEach(() => {
     StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-    jest.restoreAllMocks();
   });
 
-  it('alert is called with logging you out', () => {
-    window.alert = jest.fn();
-    shallow(<App />);
-    keydown.keydown({ key: "h", ctrlKey: true });
-    expect(window.alert).toHaveBeenCalledWith("Logging you out");
-  });
-  
-  it('logOut function should be used', () => {
-    window.alert = jest.fn();
-    const SApp = mount(<App />);
-    SApp.instance().logOut = jest.fn();
-    keydown.keydown({ key: "h", ctrlKey: true });
-    expect(SApp.instance().logOut).toHaveBeenCalled();
-  })
+  let app = mount(<App />);
+  // login the app
+  app.setState({ user: {isLoggedIn: true }});
+  const body = app.find('.App-body');
+  const login = body.find('Login');
+  const courseListRender = body.find('CourseList').render()[0];
 
+  it('without crashing', () => {
+    assert.equal(app.length, 1);
+  });
+
+  it('the CourseList', () => {
+    assert.equal(courseListRender.name, 'table');
+  });
+
+  it('NOT the Login', () => {
+    assert.equal(login.length, 0);
+  });
 });
 
-describe('State is used and set correctly', () => {
+describe('mapStateToProps', () => {
+
   beforeEach(() => {
     StyleSheetTestUtils.suppressStyleInjection();
-  })
-  afterEach(function() {
+  });
+
+  afterEach(() => {
     StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
   });
-  it('displayDrawer default state is set to false', () => {
-    const SApp = shallow(<App />);
-    expect(SApp.state().displayDrawer).toEqual(false);
+
+  it('returns the right state', () => {
+    const state = fromJS({
+      isUserLoggedIn: true,
+    });
+    const returnProp = {
+      isLoggedIn: true,
+    }
+    const props = mapStateToProps(state);
+    assert.deepEqual(props, returnProp);
   });
-  it('handleDisplayDrawer works as intended', () => {
-    const SApp = shallow(<App />);
-    SApp.instance().handleDisplayDrawer();
-    expect(SApp.state().displayDrawer).toEqual(true);
-  });
-  it('handleHideDrawer works as intended', () => {
-    const SApp = shallow(<App />);
-    SApp.instance().handleDisplayDrawer();
-    SApp.instance().handleHideDrawer();
-    expect(SApp.state().displayDrawer).toEqual(false);
-  });
+
+
 });
