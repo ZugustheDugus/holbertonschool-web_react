c1260bac5cef2a9868d03d0b100978dfb9cf19bb
ZugustheDugus <h1cooper2019@protonmail.com>
2023-02-06 17:13:55 -0600

    tasks 3-5

---
+++ 0x04-react_redux_connectors_and_providers/task_5/dashboard/src/schema/notifications.js
@@ -0,0 +1,27 @@
+import notifData from '../notifications.json';
+import { schema, normalize } from 'normalizr';
+
+
+const user = new schema.Entity('users');
+
+const message = new schema.Entity('messages', {}, {
+  idAttribute: 'guid',
+});
+
+export const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message,
+});
+
+export const normalizedData = normalize(notifData, [notification]);
+
+export function getAllNotificationsByUser(userId) {
+  return Object.values(normalizedData.entities.notifications)
+    .filter((note) => note.author == userId)
+    .map((note) => normalizedData.entities.messages[note.context]);
+}
+
+export function notificationsNormalizer(data) {
+  const normalized = normalize(data, [notification]);
+  return normalized.entities;
+}
